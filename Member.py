66a3e817555869de46905e8a0b13b424b6b9c3f5
Eoin OKennedy <Eoin.okennedy@tudublin.ie>
2022-03-22 09:06:27 +0000

    added Member.py class

---
+++ Member.py
@@ -0,0 +1,21 @@
+class Member():
+    def __init__(self,arg1,arg2,arg3,arg4):
+        self.__name=arg1
+        self.__age=arg2
+        self.__smoker=arg3
+        self.__memberType=arg4
+
+    def getName(self):
+        return self.__name
+    
+    def getAge(self):
+        return self.__age
+
+    def getSmoker(self):
+        return self.__smoker
+
+    def getMemberType(self):
+        return self.__memberType
+
+    def __str__(self):
+        return self.__name
